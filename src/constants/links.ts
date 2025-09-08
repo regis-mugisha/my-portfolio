1bb83e87bc392e763c57aca7c3a7349d25ae9875
regis-mugisha <mugisharegis72@gmail.com>
2025-09-09 00:23:40 +0200

    chore: add "Tech-Stack" link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,1 +1,8 @@
-export const links = ["Home", "About", "Projects", "Education", "Contact"];
+export const links = [
+  "Home",
+  "About",
+  "Tech-Stack",
+  "Projects",
+  "Education",
+  "Contact",
+];
