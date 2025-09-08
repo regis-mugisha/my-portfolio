93131fb47efb3b0d6360dc473422f50e81510ef0
regis-mugisha <mugisharegis72@gmail.com>
2025-09-09 00:05:49 +0200

    feat: add TechIcon component

---
+++ src/components/common/TechIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import type { Technology } from "@/types";
+
+export const TechIcon = ({ name, logo, bgClassName }: Technology) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-2">
+      <div
+        className={cn(
+          "flex h-18 w-18 items-center justify-center rounded-full",
+          bgClassName
+        )}
+      >
+        <img src={logo} alt={`${name} logo`} className="h-12 w-12" />
+      </div>
+      <p className="font-medium">{name}</p>
+    </div>
+  );
+};
