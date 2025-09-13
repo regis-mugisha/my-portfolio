bd1cf58f14fb9e708fdc7568c66b290255145396
regis-mugisha <mugisharegis72@gmail.com>
2025-09-14 00:30:19 +0200

    feat: add image placeholder component indicating no image available

---
+++ src/components/common/ImagePlaceholder.tsx
@@ -0,0 +1,11 @@
+import { ImageOff } from "lucide-react";
+
+const ImagePlaceholder = () => {
+  return (
+    <div className="w-full h-47 flex justify-center items-center border-2 rounded-2xl">
+      <ImageOff size={70} />
+    </div>
+  );
+};
+
+export default ImagePlaceholder;
