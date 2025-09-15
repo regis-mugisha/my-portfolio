5df5e625f942a32ced7328d2345529120e0cb185
regis-mugisha <mugisharegis72@gmail.com>
2025-09-15 21:59:07 +0200

    refactor

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,8 +1,1 @@
-export const links = [
-  "Home",
-  "About",
-  "Tech-Stack",
-  "Projects",
-  "Education",
-  "Contact",
-];
+export const links = ["Home", "About", "Tech-Stack", "Projects", "Contact"];
