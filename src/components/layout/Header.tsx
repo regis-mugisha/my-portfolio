0acb98111d9579ca251caeb9c8d9673f6a1997d5
regis-mugisha <mugisharegis72@gmail.com>
2025-09-17 16:18:28 +0200

    feat: transform header to be responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
-import { Moon } from "lucide-react";
 import { links } from "@/constants/links";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import { AnimatedThemeToggler } from "../magicui/animated-theme-toggler";
+import { Button } from "../ui/button";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
-    <header className="sticky top-0 z-50 w-full backdrop-blur-sm flex items-center justify-between py-2 px-35">
+    <header className="sticky top-0 z-50 w-full backdrop-blur-sm flex items-center justify-between py-4 px-6 md:px-16">
       <img
         className="w-8 h-8 rounded-full object-cover"
         src="/src/assets/images/profile-pic.jpg"
@@ -12,8 +15,7 @@ const Header = () => {
       />
 
       {/* Navigation bar */}
-      <div>
-        <nav className="flex items-center gap-7 border py-3 px-4 rounded-full shadow-md">
+        <nav className="hidden md:flex items-center gap-7 border py-3 px-4 rounded-full shadow-md">
           {links.map((link) => (
             <a
               href={`#${link.toLowerCase()}`}
@@ -24,11 +26,31 @@ const Header = () => {
             </a>
           ))}
         </nav>
-      </div>
 
-      <div>
+      <div className="flex items-center gap-4">
         <AnimatedThemeToggler />
+        <Button variant="ghost" onClick={() => setIsMenuOpen(!isMenuOpen)} className="md:hidden">
+          {isMenuOpen ? <X /> : <Menu />}
+        </Button>
       </div>
+      {isMenuOpen && (
+        <nav
+          className="absolute top-full inset-x-0 w-full mt-6 md:hidden bg-background shadow-lg rounded-lg border"
+        >
+          <div className="flex flex-col items-center justify-center gap-4 p-4">
+            {links.map((link) => (
+              <a
+                href={`#${link.toLowerCase()}`}
+                key={link}
+                onClick={() => setIsMenuOpen(false)}
+                className="hover:text-primary transition-colors text-lg"
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+        </nav>
+      )}
     </header>
   );
 };
