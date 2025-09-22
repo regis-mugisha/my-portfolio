8b37029a0221dbaab0406e1c048219efb0cc65ae
regis-mugisha <mugisharegis72@gmail.com>
2025-09-22 15:24:41 +0200

    feat: add reveal on scroll animation

---
+++ src/components/common/RevealOnScroll.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import type { PropsWithChildren } from "react";
+
+const variants = {
+  hidden: { opacity: 0, y: 16 },
+  visible: { opacity: 1, y: 0 },
+};
+
+type RevealOnScrollProps = PropsWithChildren<{
+  id: string;
+  className: string;
+}>;
+
+const RevealOnScroll = ({ children, id, className }: RevealOnScrollProps) => {
+  return (
+    <motion.section
+      id={id}
+      className={className}
+      variants={variants}
+      initial="hidden"
+      whileInView="visible"
+      transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+      viewport={{ once: true, amount: 0.25 }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export default RevealOnScroll;
