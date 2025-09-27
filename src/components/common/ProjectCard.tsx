89fd771bf898c4202afac7c9803b77f54deb43ab
regis-mugisha <mugisharegis72@gmail.com>
2025-09-27 22:38:14 +0200

    feat(mobile): toggle image overlay on tap

--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -23,9 +23,12 @@ const ProjectCard = ({
     <div>
       <Card>
         <CardContent className="flex flex-col gap-3">
-          <div className="relative overflow-hidden rounded-2xl group">
+          <div
+            className="relative overflow-hidden rounded-2xl group"
+            tabIndex={0}
+          >
             {/* Overlay */}
-            <div className="absolute inset-0 z-10 bg-black/50 transform origin-left scale-x-0 transition-all duration-200 ease-out opacity-0 group-hover:opacity-100 group-hover:scale-x-100 flex justify-center items-center gap-3">
+            <div className="absolute inset-0 z-10 bg-black/50 transform origin-left scale-x-0 transition-all duration-200 ease-out opacity-0 group-hover:opacity-100 group-hover:scale-x-100 group-focus-within:opacity-100 group-focus-within:scale-x-100 flex justify-center items-center gap-3">
               {liveDemoLink && (
                 <IconButton hrefLink={liveDemoLink}>
                   <SquareArrowOutUpRight />
